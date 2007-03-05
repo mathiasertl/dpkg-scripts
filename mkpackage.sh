6829eee05e9b6dd5a8c5237c1fa28256118ee837
Mathias Ertl <mati@fsinf.at>
2007-03-05 07:44:53 +0000

    updated the scripts to their newest versions...
    git-svn-id: https://svn.fsinf.at/repositories/scripts@56 997389ca-7026-0410-9b2b-f7b85ba8ea54

--- mkpackage.sh
+++ mkpackage.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-set -e
-
 control_dir="DEBIAN"
 
 if [[ "$1" == "" ]]; then
 	echo "Please supply the directory of the package to be build."
-	exit 1
+	exit
 fi
 
 if [[ "$2" != "" ]]; then
@@ -14,10 +11,9 @@ if [[ "$2" != "" ]]; then
 else
 	# note: if you don't specify a target, don't build this directly from
 	# the package directory
-	target="."
+	target=.
 fi
 
-# some sanity checks:
 if [[ ! -d "$1" ]]; then
 	echo "Error: $1 doesn't exist"
 	exit
@@ -43,29 +39,30 @@ package=$(grep "Package: " "$dir/$control_dir/control" | sed 's/^Package: *\(.*\
 version=$(grep "Version: " "$dir/$control_dir/control" | sed 's/^Version: *\(.*\)/\1/')
 arch=$(grep "Architecture: " "$dir/$control_dir/control" | sed 's/^Architecture: *\(.*\)/\1/')
 
-doc_dir="$dir/usr/share/doc/$package/"
+# this just ensures correct permissions for everything.
+echo -n "Fixing permissions..."
+chmod -R a-rwx $dir
+chmod -R a+rX $dir
+chmod -R u+w $dir
+if [[ -f "$dir/$control_dir/postinst" || -f "$dir/$control_dir/postrm" ]]; then
+	chmod 755 $dir/$control_dir/post*
+fi
+if [[ -f "$dir/$control_dir/preinst" || -f "$dir/$control_dir/prerm" ]]; then
+	chmod 755 $dir/$control_dir/pre*
+fi
+echo " Done."
+
+doc_dir="$dir/usr/share/doc/$package"
 if [[ -d "$doc_dir" ]]; then
 	sed -i "s/__DATE/$(822-date)/" $doc_dir/changelog
 	sed -i "s/__DATE/$(822-date)/" $doc_dir/changelog.Debian
+
+	gzip --best $doc_dir/changelog
+	gzip --best $doc_dir/changelog.Debian
 else
 	echo "ERROR: no doc dir $doc_dir found."
 	exit
 fi
-
-# since all of this is now in an svn-repository, we have to create
-# a temporary directory that does not include all the .svn directories.
-# note that we can delete it afterwards.
-if [[ -d $target/tmp ]]; then
-	echo "Error: $target/tmp already exists. Exiting."
-	exit 1
-fi
-rsync -ra --delete $dir/ $target/tmp --exclude ".svn" --exclude "DEBIAN/exec"
-dir=$target/tmp/
-doc_dir="$dir/usr/share/doc/$package"
-
-# existense of these files is already asserted by the above __DATE replacement.
-gzip --best $doc_dir/changelog
-gzip --best $doc_dir/changelog.Debian
 
 # create md5sums:
 old_dir=$(pwd)
@@ -79,35 +76,11 @@ else
 fi
 cd $old_dir
 
-# this just ensures correct permissions for everything.
-echo -e "Fixing permissions and ownership..."
-chmod -R go-rwx $dir
-chmod -R a+rX $dir
-chmod -R u+w $dir
-if [[ -f "$dir/$control_dir/postinst" || -f "$dir/$control_dir/postrm" ]]; then
-	chmod 755 $dir/$control_dir/post*
-fi
-if [[ -f "$dir/$control_dir/preinst" || -f "$dir/$control_dir/prerm" ]]; then
-	chmod 755 $dir/$control_dir/pre*
-fi
-
-# implement the feature that we can mark executables in $control_dir/exec
-if [[ -f "$dir/$control_dir/exec" ]]; then
-	for i in $(cat "$dir/$control_dir/exec"); do
-		if [[ -f "$dir/$i" ]]; then
-			chmod 755 $dir/$i
-		else
-			echo "Warning: $i is marked executable, but doesn't exist!"
-		fi
-	done
-fi
-
-echo -e "Sudo "
-sudo chown -R root:root $dir
-echo " Done."
-
 # final step
 echo "dpkg-deb --build $dir $target/"$package"_"$version"_"$arch".deb"
 dpkg-deb --build $dir $target/"$package"_"$version"_"$arch".deb
 
-sudo rm -r $dir
+# an additional little feature: changelog files are compressed before packaging
+# and decompressed afterwards (for convinience)
+gzip -d $doc_dir/changelog.gz
+gzip -d $doc_dir/changelog.Debian.gz
--- repo-maint.sh
+++ repo-maint.sh
@@ -162,4 +162,3 @@ done
 
 gpgpass=""
 echo "Finished."
-exit
