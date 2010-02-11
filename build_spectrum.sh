99f54fdca636301fb0813f07bcb7c8aa404d5019
Mathias Ertl <mati@fsinf.at>
2010-02-11 09:04:07 +0000

    only delete spectrum-dev packages, but also delete the new ones after build
    git-svn-id: https://svn.fsinf.at/repositories/scripts@483 997389ca-7026-0410-9b2b-f7b85ba8ea54

--- build_spectrum.sh
+++ build_spectrum.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm -rf /home/mati/build/*
+find | grep spectrum-dev | xargs rm 
 cd /chroot
 su mati -c "mchroot schroot -c DIR -d /home/mati/repositories/all/spectrum-dev ../../scripts/build.py"
 cd /srv/www/apt.fsinf.at/dists
 rsync -av /home/mati/build/ ./
 repo-maint
+find | grep spectrum-dev | xargs rm 
