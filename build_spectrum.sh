f2e4d4b9add8246f965a7fe9ce8234ef1955b266
Mathias Ertl <mati@fsinf.at>
2010-05-07 18:10:18 +0000

    preserve env-variables in schroot command
    git-svn-id: https://svn.fsinf.at/repositories/scripts@596 997389ca-7026-0410-9b2b-f7b85ba8ea54

--- build_spectrum.sh
+++ build_spectrum.sh
@@ -7,7 +7,7 @@ fi
 
 find /home/mati/build | grep spectrum-dev | xargs rm -f 2> /dev/null
 cd /chroot
-su mati -c "mchroot schroot -c DIR -d /home/mati/repositories/all/spectrum-dev ../../scripts/build.py"
+su mati -c "mchroot schroot -p -c DIR -d /home/mati/repositories/all/spectrum-dev ../../scripts/build.py"
 cd /srv/www/apt.fsinf.at/dists
 rsync -av /home/mati/build/ ./
 repo-maint
