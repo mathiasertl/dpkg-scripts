427940c8fea4b8ff79afdf7d02ad2cedd896adbb
Mathias Ertl <mati@fsinf.at>
2011-12-02 17:13:06 +0100

    add build_spectrum2.sh

---
+++ build_spectrum2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $(whoami) != "root" ]]; then
+	echo "Error: require root privileges."
+	exit 1 
+fi
+
+APT_REPO="/srv/www/apt.fsinf.at/dists/"
+
+set -x
+
+# remove old packages:
+find /home/mati/build | grep spectrum2.*git | xargs rm -f 2> /dev/null
+find /home/mati/build | grep libtransport.*git | xargs rm -f 2> /dev/null
+# remove empty directories
+find -depth -empty -type d -exec rmdir {} \;
+
+# build packages
+cd /chroot
+su mati -c "mchroot schroot -p -c DIR -d /home/mati/repositories/spectrum2 build.py spectrum2-git"
+
+# update repositories
+rsync --include='libtransport*git*' --include='spectrum2*git*' --exclude='*.*' -av /home/mati/build/ $APT_REPO
+repo-maint
+
+# remove packages again:
+find /home/mati/build | grep spectrum2.*git | xargs rm -f 2> /dev/null
+find /home/mati/build | grep libtransport.*git | xargs rm -f 2> /dev/null
+# remove empty directories
+find $APT_REPO -depth -empty -type d -exec rmdir {} \;
