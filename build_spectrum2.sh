89e93dceb2cde4ccfb02337ae8cec61d3a5f121b
Mathias Ertl <mati@fsinf.at>
2012-08-10 14:30:19 +0200

    various improvements from over time (never committed, sorry for that!)

--- build_spectrum2.sh
+++ build_spectrum2.sh
@@ -6,25 +6,40 @@ if [[ $(whoami) != "root" ]]; then
 fi
 
 APT_REPO="/srv/www/apt.fsinf.at/dists/"
+DPKG_REPO='/home/mati/repositories/spectrum2'
 
 set -x
 
+cd $DPKG_REPO
+su mati -c 'git checkout upstream'
+su mati -c 'git pull'
+
+yesterday=$(date --date=yesterday)
+no_commits=$(su mati -c 'git log --oneline --since="`date --date=yesterday`"' | wc -l)
+
+version=$(git describe --tags --match '[0-9].*' | sed 's/\(^[0-9\.]*\)-/\1~/')
+
+su mati -c 'git checkout master'
+
+if [[ $no_commits -eq 0 && -z $FORCE_BUILD ]]; then
+	exit
+fi
+
+su mati -c 'git merge upstream'
+
+sed -i "1s/\((.*)\)/(1:$version-1)/" debian/changelog
+
 # remove old packages:
-find /home/mati/build | grep spectrum2.*git | xargs rm -f 2> /dev/null
-find /home/mati/build | grep libtransport.*git | xargs rm -f 2> /dev/null
-# remove empty directories
-find /home/mati/build -depth -empty -type d -exec rmdir {} \;
+find /home/mati/build | grep ^spectrum2 | xargs rm -f 2> /dev/null
+find /home/mati/build | grep ^libtransport | xargs rm -f 2> /dev/null
 
 # build packages
-cd /chroot
-su mati -c "mchroot schroot -p -c DIR -d /home/mati/repositories/spectrum2 build.py spectrum2-git"
+su mati -c "mchroot --fd=lucid git-build.py"
 
 # update repositories
-rsync --include='libtransport*git*' --include='spectrum2*git*' --exclude='*.*' -av /home/mati/build/ $APT_REPO
+rsync --include='libtransport*' --include='spectrum2*' --exclude='*.*' -av /home/mati/build/ $APT_REPO
 repo-maint
 
 # remove packages again:
-find /home/mati/build | grep spectrum2.*git | xargs rm -f 2> /dev/null
-find /home/mati/build | grep libtransport.*git | xargs rm -f 2> /dev/null
-# remove empty directories
-find /home/mati/build -depth -empty -type d -exec rmdir {} \;
+find /home/mati/build | grep ^spectrum2 | xargs rm -f 2> /dev/null
+find /home/mati/build | grep ^libtransport | xargs rm -f 2> /dev/null
