d939bdd2308c88c3dfd1e4df0dd7911491026439
Mathias Ertl <mati@er.tl>
2018-08-31 14:29:45 +0200

    match message to what upstream maintainers use

--- erlang-p1-update.sh
+++ erlang-p1-update.sh
@@ -22,9 +22,9 @@ git checkout debian
 git merge master -m "Merge branch 'master' into debian"
 
 if [[ $(head -n 1 debian/changelog | grep -c "1:") -eq "1" ]]; then
-    dch -v 1:${VERSION}-0.1 -D unstable "New upstream release."
+    dch -v 1:${VERSION}-0.1 -D unstable "New upstream version ${VERSION}"
 else
-    dch -v ${VERSION}-0.1 -D unstable "New upstream release."
+    dch -v ${VERSION}-0.1 -D unstable "New upstream version ${VERSION}"
 fi
 git commit debian/changelog -m "update changelog with new version ${VERSION}"
 git-export-orig
