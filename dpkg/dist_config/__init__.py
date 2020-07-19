64e1a443b7bb4cd94f04601475db13d82cc79f09
Mathias Ertl <mati@er.tl>
2020-07-19 19:51:54 +0200

    add function to load all distros

--- dpkg/dist_config/__init__.py
+++ dpkg/dist_config/__init__.py
@@ -10,25 +10,41 @@ from ..utils import parse_date
 config = {}
 
 
-def load_distributions():
+def load_distributions(skip=None, start=None, until=None):
+    if skip is None:
+        skip = []
+
+    def _sort_dist_key(t):
+        val = t[1]['DEFAULT']['release-date']
+        if val == '':
+            return ('2099-12-31', t[0])
+        return (val, t[0])
+
     today = date.today()
 
+    # get config files in dpkg.dist_config module
     dists = [os.path.splitext(f)[0] for f in resources.contents('dpkg.dist_config') if f.endswith('.cfg')]
-    for dist in list(sorted(dists)):
-        config = get_config(dist)
 
+    # load config files
+    dists = {d: get_config(d) for d in dists if d not in skip}
+
+    # filter config files with skip=True
+    dists = {d: c for d, c in dists.items() if not c['DEFAULT'].getboolean('skip', fallback=False)}
+
+    for dist, config in sorted(dists.items(), key=_sort_dist_key):
         supported_until = config['DEFAULT']['supported-until']
         if supported_until == '':
             continue  # EOL for this dist has not been defined yet
+        elif today > parse_date(supported_until):
+            del dists[dist]
 
-        if today > parse_date(supported_until):
-            dists.remove(dist)
-            continue
+    dists = [d for d, c in sorted(dists.items(), key=_sort_dist_key)]
+    if start:
+        dists = dists[dists.index(start):]
+    if until:
+        dists = dists[:dists.index(until) + 1]
 
-        if config['DEFAULT'].getboolean('skip'):
-            dists.remove(dist)
-
-    print(sorted(dists))
+    return dists
 
 
 def get_config(dist):
